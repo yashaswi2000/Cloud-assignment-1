cbdb60a2c1d5b479275d14f80c6a99b100fad26c
Yashaswi Makula <yashaswi.makula@gmail.com>
2024-02-22 18:01:37 -0500

    adding Lf2

---
+++ elastichelper.py
@@ -0,0 +1,59 @@
+import json
+import requests
+from requests_aws4auth import AWS4Auth
+from opensearchpy import OpenSearch, RequestsHttpConnection
+
+AWS_ACCESS_KEY = 'AKIAW3MEA63CKPN2MMLB'
+AWS_SECRET_KEY = '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'
+region = 'us-east-1'
+service = 'es'
+
+awsauth = AWS4Auth(AWS_ACCESS_KEY, AWS_SECRET_KEY, region, service)
+
+host = 'search-elasticstarsearch-3lcridy75e33yvzpzhfh2r4n5i.us-east-1.es.amazonaws.com' # For example, my-test-domain.us-east-1.es.amazonaws.com
+
+es = OpenSearch(
+    hosts = [{'host': host, 'port': 443}],
+    http_auth = awsauth,
+    use_ssl = True,
+    verify_certs = True,
+    connection_class = RequestsHttpConnection
+)
+
+print(es.info())
+
+
+
+# response = es.indices.create(index='restaurants',
+#                              body={
+#                                  'mappings': {
+#                                      'properties': {
+#                                      'Restaurant': {
+#                                         'properties': {
+#                                             'RestaurantID': {'type': 'text'},
+#                                             'Cuisine': {'type': 'text'},
+#                                         }
+#                                     }
+#                                  }
+#                              }})
+
+for cusine in ['Indian', 'Chinese', 'Mexican']:
+    file = open(cusine+'.json', 'r')
+    data = json.loads(file.read())
+    file.close()
+    for i in range(len(data)):
+        response = es.index(    
+            index='restaurants',
+            body={
+                'Restaurant': {
+                    'RestaurantID': data[i]['id'],
+                    'Cuisine': cusine,
+                }
+            },
+            id=data[i]['id'],
+        )
+        print(response)
+
+    res = es.search(index='restaurants', body={'query': {'match_all': {}}})
+
+    print(res)
